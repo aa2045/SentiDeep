74dc2a00db9373cbe9a9ddbd6fa90b843ea134b9
aa2045 <72710454+aa2045@users.noreply.github.com>
2021-10-16 19:01:35 +0400

    Delete cli_main.py

--- cli_main.py
+++
@@ -1,7 +0,0 @@
-from cli_controller import Controller
-
-"""Application to train deep-learning classifiers for Sentiment Analysis
-"""
-if __name__ == '__main__':
-    controller = Controller()
-    controller.run()
